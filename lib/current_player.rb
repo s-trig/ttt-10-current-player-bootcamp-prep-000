eff4884d8d9a5ec0a516e60dc9d6e0acd12ef784
David Reep <davereep@gmail.com>
2017-10-25 19:04:12 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,11 @@
 def turn_count(board)
   count = 0
   board.each do |turn|
-      if board[turn] == "X" || board[turn] == "O"
-        count += 1
-      end
+    if board[turn] == "X" || board[turn] == "O"
+      count += 1
+    end
   end
   return count
 end
+
+
